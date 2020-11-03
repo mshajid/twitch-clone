803dcdd0e5856f2e002c65ce6885965b17aee6b9
mshajid <70450142+mshajid@users.noreply.github.com>
2020-11-03 13:44:09 +0530

    Committed

--- src/Body.js
+++ src/Body.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Profile from './Profile';
 import "./Body.css";
+import Stream from "./Stream";
 
 const Body = () => {
     return (
         <div className="body">
             <div className="body--left">
+                <Stream />
                 <Profile />
+                
             </div>
             <div className="body__rightPlaceHolder">
 
