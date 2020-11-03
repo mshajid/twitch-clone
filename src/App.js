a3380dea1b54ab9462dc8b495f76461e6102333e
mshajid <70450142+mshajid@users.noreply.github.com>
2020-11-03 13:43:24 +0530

    Added Stream Imported

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import './App.css';
 import Body from './Body';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import Stream from './Stream';
 
 function App() {
   return (
     //BEM Naming
     <div className="app">
+      
       <Header />
+      
       <div className="app__main">
         <Sidebar />
         <Body />
       </div>
+     
     </div>
   );
 }
